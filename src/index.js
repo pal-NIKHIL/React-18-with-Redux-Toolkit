d4481bac249bbf39eef96d1dd945ba62bd293053
pal-NIKHIL <94384228+pal-NIKHIL@users.noreply.github.com>
2023-04-08 18:43:48 +0530

    day 2

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-const movie1={
-    poster:"https://i.pinimg.com/originals/18/57/b1/1857b137d5435e0da29a2410d4935b21.jpg",
-    name:"JADOO"
+function alertmessage()
+{
+    return alert("PAYMENT DONE")
 }
-const movie2={
-    poster:"https://images.squarespace-cdn.com/content/v1/5a608e4e017db2e8dedb242b/1614669829730-HVBQPR6LNS5QEKE2JM6N/IMG_1399.jpg",
-    name:"HUM"
-}
-const movie3={
-    poster:"https://i.tribune.com.pk/media/images/1277601-dangal-1482925425/1277601-dangal-1482925425.jpg",
-    name:"DANGAL"
-}
+const moviedata = [
+    {
+        poster: "https://i.pinimg.com/originals/18/57/b1/1857b137d5435e0da29a2410d4935b21.jpg",
+        name: "JADOO",
+        id:"1"
+    },
+    {
+        poster: "https://images.squarespace-cdn.com/content/v1/5a608e4e017db2e8dedb242b/1614669829730-HVBQPR6LNS5QEKE2JM6N/IMG_1399.jpg",
+        name: "HUM",
+        id:"2"
+    },
+    {
+        poster: "https://i.tribune.com.pk/media/images/1277601-dangal-1482925425/1277601-dangal-1482925425.jpg",
+        name: "DANGAL",
+        id:"3"
+    }
+]
+
 const MovieList = () => {
     return (
-        <div className="container">
-            <Movie poster={movie1.poster} name={movie1.name}/>
-            <Movie poster={movie2.poster} name={movie2.name}/>
-            <Movie poster={movie3.poster} name={movie3.name}/>
+        <div className="main">
+                <h1>Top 10 Movie of all time</h1>
+                <div className="container">
+                    {moviedata.map((movie) => {
+                        return <Movie {...movie} key={movie.id}></Movie>
+
+                    })
+                    }
+                </div>
+            <EventExample/>
         </div>
     );
 };
-
-const Movie = ({poster,name}) => {
+const Movie = ({name, poster}) => {
     return (
         <article className="book">
             <img src={poster}></img>
@@ -31,6 +46,20 @@ const Movie = ({poster,name}) => {
         </article>
     );
 };
+const EventExample = ()=>{
+    const totalamount = (e)=>{
+        e.preventDefault()
+        console.log(e)
+
+    }
+    return <div>
+        <form onSubmit={totalamount}>
+            <h1>Quantity</h1>
+            <input type='text' name="data" id="quantity"/>
+            <button>$ 10.55 </button>
+        </form>
+    </div>
+}
 
 const data = ReactDOM.createRoot(document.getElementById("root"));
-data.render(<MovieList />);
+data.render(<MovieList/>);
